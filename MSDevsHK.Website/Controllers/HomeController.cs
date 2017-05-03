0782eb2438e096eb7c2154fc9b5666f2c0390825
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2017-05-03 15:47:08 +0800

    Rename source folder

--- MSDevsHK.Website/Controllers/HomeController.cs
+++ MSDevsHK.Website/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace source.Controllers
+namespace MSDevsHK.Website.Controllers
 {
     public class HomeController : Controller
     {
--- MSDevsHK.Website/Program.cs
+++ MSDevsHK.Website/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace source
+namespace MSDevsHK.Website
 {
     public class Program
     {
