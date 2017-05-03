f6d8b0e4aefef2c2b255ef6906d64f02ce9d3f3e
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2017-05-03 13:02:39 +0800

    Upgrade to ASP.NET Core 1.1 MVC

--- source/Program.cs
+++ source/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace MSDevsHK.Website
+namespace source
 {
     public class Program
     {
